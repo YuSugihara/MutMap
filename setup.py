defc50180291e712b451432fcce5cb5209c9112d
Yu Sugihara <yu57th@gmail.com>
2019-06-06 18:25:54 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MutMap',
+      version='2.0.3',
+      description='MutMap: pipeline to identify causative mutations responsible for a phenotype',
+      author='Yu Sugihara',
+      author_email='yu57th@gmail.com',
+      url='https://github.com/YuSugihara/MutMap',
+      license='GPL',
+      packages=['mutmap'],
+     )
