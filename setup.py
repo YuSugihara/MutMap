8df7c09f29edc7394218cf40a5af3bc858dd770d
Yu Sugihara <yu57th@gmail.com>
2019-06-09 14:44:23 +0900

    fix version

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from mutmap.__init__ import __version__
 
 setup(name='mutmap',
-      version='2.0.4',
+      version='{}'.format(__version__),
       description='MutMap: pipeline to identify causative mutations responsible for a phenotype',
       author='Yu Sugihara',
       author_email='yu57th@gmail.com',
