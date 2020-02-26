fb6e96b78106a1b136c7a4a60ccbd0f3babcca04
Yu Sugihara <yu57th@gmail.com>
2020-02-26 15:56:04 +0900

    add test dataset

---
+++ test/mutmap.sh
@@ -0,0 +1,7 @@
+mutmap -o test \
+       -n 20 \
+       -w 100 \
+       -s 20 \
+       -r mutmap_ref.fasta \
+       -c mutmap_cultivar.1.fastq.gz,mutmap_cultivar.2.fastq.gz \
+       -b mutmap_bulk.1.fastq.gz,mutmap_bulk.2.fastq.gz
