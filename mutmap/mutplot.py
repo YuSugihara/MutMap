2c8db6c1da7097e5b1fa4ba44c7ed61e5b26a2e8
Yu Sugihara <yu57th@gmail.com>
2024-10-18 11:38:37 +0900

    updated

--- mutmap/mutplot.py
+++ mutmap/mutplot.py
@@ -10,12 +10,6 @@ import pandas as pd
 import subprocess as sbp
 from multiprocessing import set_start_method
 set_start_method('fork')
-# These environment variables can avoid the potential errors
-# in python and java
-if os.environ.get('OMP_NUM_THREADS') == None:
-    os.environ['OMP_NUM_THREADS'] = str(args.threads)
-if os.environ.get('USE_SIMPLE_THREADED_LEVEL3') == None:
-    os.environ['USE_SIMPLE_THREADED_LEVEL3'] = str(args.threads)
 if os.environ.get('JAVA_TOOL_OPTIONS') == None:
     os.environ['JAVA_TOOL_OPTIONS'] = '-Xmx4000m'
 from mutmap.vcf2index import Vcf2Index
